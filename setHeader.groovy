bb65ede214bf1095c8823e1e4c8dac8dd442cc87
stephenxue <xiaoyu.xue@gmail.com>
2020-02-03 14:42:09 +1100

    programs 02

---
+++ setHeader.groovy
@@ -0,0 +1,7 @@
+﻿import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+def Message processData(Message message) {
+  message.setHeader("Content-Type", "text/xml" + "; charset=utf-8" );
+  message.setHeader("Accept", "text/xml" + "; charset=utf-8" );
+  return message;
+}
